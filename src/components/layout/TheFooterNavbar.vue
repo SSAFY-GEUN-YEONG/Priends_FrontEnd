7db828c11b615c1998d0fa53486e29f74f0fca5b
hggew <hggew@naver.com>
2023-11-17 09:22:14 +0900

    feat : footer 수정

--- src/components/layout/TheFooterNavbar.vue
+++ src/components/layout/TheFooterNavbar.vue
@@ -4,22 +4,22 @@ import "@/assets/css/footer.css";
 
 <template>
   <footer class="navbar-fixed-bottom mt-5">
-    <div class="row justify-content-center">
-      <div class="footer-content col-md-2">
-        <div class="footer-title">Priends</div>
+    <div class="d-flex flex-row justify-content-evenly footer-big pt-2">
+      <div class="" style="width: 15%">
+        <div class="footer-title my-1 pb-1">Priends</div>
         <p>여행지</p>
         <p>여행 계획</p>
         <p>커뮤니티</p>
         <p>동행</p>
       </div>
-      <div class="footer-content col-md-2">
-        <div class="footer-title">회사이야기</div>
+      <div class="" style="width: 15%">
+        <div class="footer-title my-1 pb-1">회사이야기</div>
         <p>회사소개</p>
         <p>이용방법</p>
         <p>광고 및 제휴</p>
       </div>
-      <div class="footer-content col-md-2">
-        <div class="footer-title">고객센터</div>
+      <div class="" style="width: 15%">
+        <div class="footer-title my-1 pb-1">고객센터</div>
         <p>FAQ</p>
         <p>문의하기</p>
         <p>이용약관</p>
@@ -27,9 +27,9 @@ import "@/assets/css/footer.css";
       </div>
     </div>
 
-    <div class="div-footer-bottom d-flex">
-      <p>Copyright ⓒ 2023 Priends.com, All Rights Reserved.</p>
-      <p>제휴문의: Priends@ssafy.com | 02-1111-2222</p>
+    <div class="d-flex flex-row justify-content-around footer-bottom">
+      <p class="m-1">Copyright ⓒ 2023 Priends.com, All Rights Reserved.</p>
+      <p class="m-1">제휴문의: Priends@ssafy.com | 02-1111-2222</p>
     </div>
   </footer>
 </template>
@@ -39,33 +39,23 @@ import "@/assets/css/footer.css";
 .navbar-fixed-bottom {
   position: relative;
   bottom: 0;
-  width: 100%;
+  width: auto;
 }
 * {
   font-size: 10px;
 }
-.row {
-  display: flex;
-  flex-wrap: wrap;
-  margin-right: -15px;
-  margin-left: -15px;
+.footer-big {
   color: #ffffff;
   background-color: #363c48;
 }
 .footer-title {
   font-weight: 700;
-  height: 30px;
   border-bottom: 1px solid #424753;
   color: #999999; /* 원하는 색상으로 변경하세요 */
-  margin: 10px auto; /* 상하 여백을 추가할 수 있습니다. */
 }
 
-.div-footer-bottom {
+.footer-bottom {
   background-color: #2e333d;
   color: #c9c9c9;
-}
-
-.div-footer-bottom > p {
-  margin: 0 auto;
 }
 </style>
