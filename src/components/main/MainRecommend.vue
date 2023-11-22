fe98fb8aeb290b412f1da4ace9ea9ab46bb3f5af
동긔 <tlsehdrms124@naver.com>
2023-11-22 21:34:42 +0900

    feat: 메인 화면에서 관광지 랜덤 4개 조회 및 상세 페이지로 이동 완료

--- src/components/main/MainRecommend.vue
+++ src/components/main/MainRecommend.vue
@@ -2,18 +2,18 @@
 import { onMounted, ref } from "vue";
 import MainRecommendAttractionItem from "./item/MainRecommendAttractionItem.vue";
 import MainRecommendPathItem from "./item/MainRecommendPathItem.vue";
-import { topGetAttractionListApi } from "@/api/attractionApi";
+import { topGetAttractionListApi, recommendGetAttractionListApi } from "@/api/attractionApi";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-const attractions = ref([]); // 인기 관광지 목록을 담을 반응형 변수
-
+const popularAttracions = ref([]); // 인기 관광지 목록을 담을 반응형 변수
+const recommendAttracions = ref([]);  // 랜덤한 관광지 목록을 담을 반응형 변수
 
 // API로부터 인기 관광지 목록을 가져오는 함수
 const fetchTopAttractions = () => {
   topGetAttractionListApi(
     ({ data }) => {
-      attractions.value = data.dataBody;
+      popularAttracions.value = data.dataBody;
     },
     (error) => {
       console.error(error);
@@ -21,24 +21,39 @@ const fetchTopAttractions = () => {
   );
 };
 
+// API로부터 랜덤한 관광지 목록을 가져오는 함수
+const fetchRecommendAttractions = () => {
+  recommendGetAttractionListApi(
+    ({ data }) => {
+      recommendAttracions.value = data.dataBody;
+    },
+    (error) => {
+      console.error(error);
+    }
+  )
+}
+
 const navigateToAttractionDetail = (contentId) => {
   router.push({ name: 'attraction-area-detail', params: { attractionid: contentId } });
 };
 
-// 컴포넌트가 마운트될 때 인기 관광지 목록을 가져옵니다.
-onMounted(fetchTopAttractions);
+// 컴포넌트가 마운트될 때 인기 관광지 목록과 랜덤한 관광지 목록을 가져옴
+onMounted(() => {
+  fetchTopAttractions();
+  fetchRecommendAttractions();
+});
 </script>
 
 <template>
   <div class="container d-flex flex-column align-items-center">
     <div class="my-5">
-      <p class="px-3 mb-0 fs-3 fw-bold">다음 휴가를 위한 인기 여행지</p>
+      <p class="px-3 mb-24 fs-3 fw-bold">다음 휴가를 위한 인기 여행지</p>
       <div
         class="recommend-attraction px-3 d-flex flex-row justify-content-between"
       >
         <!-- API로부터 가져온 인기 관광지 목록을 반복하여 표시 -->
         <MainRecommendAttractionItem
-          v-for="attraction in attractions"
+          v-for="attraction in popularAttracions"
           :key="attraction.content_id"
           :title="attraction.title"
           :image="attraction.first_image"
@@ -48,14 +63,18 @@ onMounted(fetchTopAttractions);
     </div>
 
     <div class="my-5">
-      <p class="px-3 mb-0 fs-3 fw-bold">이번 주말을 위한 무작위 여행지</p>
+      <p class="px-3 mb-24 fs-3 fw-bold">이번 주말을 위한 무작위 여행지</p>
       <div
         class="recommend-attraction px-3 d-flex flex-row justify-content-between mb-5"
       >
-        <MainRecommendPathItem />
-        <MainRecommendPathItem />
-        <MainRecommendPathItem />
-        <MainRecommendPathItem />
+        <!-- API로부터 가져온 랜덤한(추천하는) 관광지 목록을 반복하여 표시 -->
+        <MainRecommendAttractionItem
+          v-for="attraction in recommendAttracions"
+          :key="attraction.content_id"
+          :title="attraction.title"
+          :image="attraction.first_image"
+          @click="navigateToAttractionDetail(attraction.content_id)"
+        />
       </div>
     </div>
   </div>
--- src/components/main/item/MainRecommendAttractionItem.vue
+++ src/components/main/item/MainRecommendAttractionItem.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 
 <template>
   <div class="main-recommend-attraction-item">
-    <div class="my-5 recommend-attraction-cover d-flex align-items-end" :style="{ backgroundImage: `url(${props.image})` }">
+    <div class="my recommend-attraction-cover d-flex align-items-end" :style="{ backgroundImage: `url(${props.image})` }">
       <div class="ps-3">
         <p class="text-white fw-bolder">{{ props.title }}</p>
       </div>
@@ -22,11 +22,12 @@ const props = defineProps({
   height: 250px;
   /* background-image: url("@/assets/img/field.jpg"); */
   background-size: cover;
+  cursor: pointer; /* 마우스 오버 시 커서를 포인터로 변경 */
+
 }
 
 /* MainRecommendAttractionItem 컴포넌트에 대한 hover 스타일 */
 .main-recommend-attraction-item:hover {
-  background-color: #f0f0f0; /* 마우스 오버 시 배경색 변경 */
   cursor: pointer; /* 마우스 오버 시 커서를 포인터로 변경 */
   border: 1px solid #d0d0d0; /* 테두리 추가 */
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* 그림자 효과 추가 */
