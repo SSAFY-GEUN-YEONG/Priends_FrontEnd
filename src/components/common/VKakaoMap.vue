583bf9ffaa54c82026478e2e49af4dd67035b75d
동긔 <tlsehdrms124@naver.com>
2023-11-22 16:13:15 +0900

    feat: 지도에 폴리라인 뜨는 기능 개발

--- src/components/common/VKakaoMap.vue
+++ src/components/common/VKakaoMap.vue
@@ -4,6 +4,7 @@ import { ref, watch, onMounted } from 'vue';
 const map = ref(null);
 const markers = ref(new Map()); // 마커를 저장할 Map 객체
 const overlays = ref(new Map()); // 오버레이를 저장할 Map 객체
+const polyline = ref(null);
 
 const props = defineProps({
   attractions: Array
@@ -39,7 +40,7 @@ const initMap = () => {
   };
 
   map.value = new kakao.maps.Map(container, options);
-  displayMarkers(); // 지도가 초기화된 후에 마커를 표시
+  // displayMarkers(); // 지도가 초기화된 후에 마커를 표시
 };
 
 const loadKakaoMapScript = () => {
@@ -49,15 +50,20 @@ const loadKakaoMapScript = () => {
   document.head.appendChild(script);
 };
 
+
 const clearMarkersAndOverlays = () => {
   markers.value.forEach(marker => marker.setMap(null));
   markers.value.clear();
   overlays.value.forEach(overlay => overlay.setMap(null));
   overlays.value.clear();
+  if (polyline.value) {
+    polyline.value.setMap(null);  // 폴리라인도 제거
+  }
 };
 
 
-const displayMarkers = () => {
+// 마커, 오버레이, 폴리라인을 표시하는 함수
+const displayMarkersAndPolyline  = () => {
   if (!window.kakao || !window.kakao.maps) {
     console.error("카카오맵 API 아직 호출 안됨!!!");
     return;
@@ -65,13 +71,19 @@ const displayMarkers = () => {
 
   clearMarkersAndOverlays();
 
+  let path = [];
+
   props.attractions.forEach(attraction => {
     const position = new kakao.maps.LatLng(attraction.latitude, attraction.longitude);
+    // 마커 생성 설정
     const marker = new kakao.maps.Marker({
       map: map.value,
-      position: position
+      position: position,
+      // 마커에 숫자 표시하기 (추가적인 커스텀)
     });
     markers.value.set(attraction.content_id, marker);
+
+    // 커스텀 오버레이 부분
     const content = `
     <div class="wrap">
       <div class="info">
@@ -90,13 +102,26 @@ const displayMarkers = () => {
 
     overlays.value.set(attraction.content_id, overlay);
     map.value.setCenter(position);
+
+    path.push(position);
   });
+
+  polyline.value = new kakao.maps.Polyline({
+    path: path, // 폴리라인 경로 설정
+    strokeWeight: 5, // 선 두께
+    strokeColor: '#FFAE00', // 선 색상
+    strokeOpacity: 1, // 선 투명도
+    strokeStyle: 'solid' // 선 스타일
+  });
+  polyline.value.setMap(map.value); // 폴리라인 지도에 표시
 };
 
+// 값 변경될때 마다 실행
 watch(() => props.attractions, (newAttractions) => {
-  displayMarkers();
+  displayMarkersAndPolyline();
 }, { deep: true });
 
+// 해당 컴포넌트 처음 마운트 될 때 실행
 onMounted(() => {
   if (!window.kakao || !window.kakao.maps) {
     loadKakaoMapScript();
