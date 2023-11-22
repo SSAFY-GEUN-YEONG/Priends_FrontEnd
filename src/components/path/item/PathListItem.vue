cf79a1de8960909a9d67b70cbdf31b71c264c35b
hggew <hggew@naver.com>
2023-11-22 09:11:47 +0900

    feat:계획조회 list item 완료

--- src/components/path/item/PathListItem.vue
+++ src/components/path/item/PathListItem.vue
@@ -1,9 +1,24 @@
 <script setup>
-import { defineProps } from "vue";
+import { ref, defineProps, onMounted } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 const props = defineProps(["pathInfo"]);
 
-console.log("path list item ", props.pathInfo);
+const period = ref(0);
+onMounted(() => {
+  console.log("path list item ", props.pathInfo);
+  period.value = calcDate();
+});
+
+const calcDate = () => {
+  // 문자열을 Date 객체로 변환
+  const date1 = new Date(props.pathInfo.startDate);
+  const date2 = new Date(props.pathInfo.endDate);
+
+  // 두 날짜 간의 차이를 밀리초로 얻고 일로 변환
+  const dayDifference = Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+  console.log("day :::", dayDifference);
+  return dayDifference;
+};
 </script>
 
 <template>
@@ -12,11 +27,13 @@ console.log("path list item ", props.pathInfo);
     <div class="p-2">
       <h5>{{ pathInfo.title }}</h5>
 
-      <p>{{ pathInfo.startDate }} - {{ pathInfo.endDate }}| 5 Days</p>
+      <p>
+        {{ pathInfo.startDate }} - {{ pathInfo.endDate }} | {{ period }} Days
+      </p>
 
       <div class="icon-text">
         <font-awesome-icon icon="fa-regular fa-user" style="height: 15px" />
-        <div>ssafy@ssafy.com</div>
+        <div>{{ pathInfo.memberNickname }}</div>
         <div class="heart-icon">
           <font-awesome-icon :icon="['far', 'eye']" style="height: 15px" />
           <div>{{ pathInfo.hit }}</div>
